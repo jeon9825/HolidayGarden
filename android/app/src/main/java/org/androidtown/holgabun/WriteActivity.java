6fa82f62821477413a4c3dd8f6438209a5315c8d
jaeryo2357@naver.com <Jaeryo@DESKTOP-EJJGE7H.localdomain>
2018-07-16 18:09:06 +0900

    이미지 업로드 완성

--- android/app/src/main/java/org/androidtown/holgabun/WriteActivity.java
+++ android/app/src/main/java/org/androidtown/holgabun/WriteActivity.java
@@ -1,13 +1,17 @@
 package org.androidtown.holgabun;
 
 import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -15,11 +19,23 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class WriteActivity extends AppCompatActivity {
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class WriteActivity extends AppCompatActivity implements View.OnClickListener{
     private static final int SELECT_PICTURE = 1;
 
     private String selectedImagePath;
+    private Bitmap bitmap;
+    private Uri selectedImageUri;
+    private ImageView image;
+    private Button RequestURL;
+    private Button TimelineButton;
+    private EditText edittext;
 
 
     @Override
@@ -27,6 +43,8 @@ public class WriteActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write);
 
+        RequestURL=(Button)findViewById(R.id.checkButton);
+        TimelineButton=(Button)findViewById(R.id.timeline_img);
         findViewById(R.id.browsePictureButton)
                 .setOnClickListener(new View.OnClickListener() {
 
@@ -39,52 +57,77 @@ public class WriteActivity extends AppCompatActivity {
                                 "Select Picture"), SELECT_PICTURE);
                     }
                 });
-        final EditText edittext=(EditText)findViewById(R.id.edittext);
-        Button button=(Button)findViewById(R.id.checkButton);
-        @SuppressLint("WrongViewCast") final TextView textView=(TextView)findViewById(R.id.textview);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                textView.setText(edittext.getText());
-            }
-        });
+        edittext=(EditText)findViewById(R.id.edittext);
+        image=(ImageView)findViewById(R.id.img_date2);
+        RequestURL.setOnClickListener(this);
+
+
 
     }
-
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {//선택한 사진 가져오기
         if (resultCode == RESULT_OK) {
-            if (requestCode == SELECT_PICTURE) {
-                Uri selectedImageUri = data.getData();
-                selectedImagePath = getPath(selectedImageUri);
+            if (requestCode == SELECT_PICTURE&&resultCode==RESULT_OK&&data !=null&&data.getData() !=null) {
+
+                selectedImageUri = data.getData();
+                try{
+                    bitmap =MediaStore.Images.Media.getBitmap(getContentResolver(),selectedImageUri);
+                    image.setImageBitmap(bitmap);
+                }catch(IOException e){
+                    e.printStackTrace();
+                }
             }
         }
     }
 
-    /**
-     * 사진의 URI 경로를 받는 메소드
-     */
-    public String getPath(Uri uri) {
-        // uri가 null일경우 null반환
-        if( uri == null ) {
-            return null;
+ public String getStringImage(Bitmap bmp){
+     ByteArrayOutputStream baos=new ByteArrayOutputStream();
+     bmp.compress(Bitmap.CompressFormat.JPEG,100,baos);
+     byte[] imageBytes =baos.toByteArray();
+     String encodedImage = Base64.encodeToString(imageBytes,Base64.DEFAULT);
+     return encodedImage;
+ }
+
+
+
+    private void uploadImage(){
+        class UploadImage extends AsyncTask<Bitmap,Void,String> {
+
+            ProgressDialog loading;
+            RequestHandler rh = new RequestHandler();
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(WriteActivity.this, "Uploading...", null,true,true);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                Toast.makeText(getApplicationContext(),s, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            protected String doInBackground(Bitmap... params) {
+                Bitmap bitmap = params[0];
+                String uploadImage = getStringImage(bitmap);
+
+                HashMap<String,String> data = new HashMap<>();
+
+                data.put("image", uploadImage);//php에서 POST값으로 들어감
+                data.put("mean",edittext.getText().toString());
+                String result = rh.sendPostRequest("http://ec2-13-209-68-163.ap-northeast-2.compute.amazonaws.com/PhotoUpload/upload.php",data);
+
+                return result;
+            }
         }
-        // 미디어스토어에서 유저가 선택한 사진의 URI를 받아온다.
-        String[] projection = { MediaStore.Images.Media.DATA };
-        Cursor cursor = managedQuery(uri, projection, null, null, null);
-        if( cursor != null ){
-            int column_index = cursor
-                    .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-            cursor.moveToFirst();
-            return cursor.getString(column_index);
-        }
-        // URI경로를 반환한다.
-        return uri.getPath();
+
+        UploadImage ui = new UploadImage();
+        ui.execute(bitmap);
     }
 
-    public void onButtonFeed(View v) { //feed 페이지로 이동
-        Intent intent = new Intent(this, FeedFragment.class);
-        startActivity(intent);
-    }
 
 
 
@@ -92,16 +135,27 @@ public class WriteActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
-    public void TimeLineClicked(View v){ //메인페이지로 이동
-        Intent intent = new Intent(this, TimeLine.class);
-        startActivity(intent);
-    }
+
     public void onClickedBackPopup(View v) {
         //데이터 담아서 팝업(액티비티) 호출
         Intent intent = new Intent(this, BackPopupActivity.class);
         intent.putExtra("data", "돌아가면 내용이 지워집니다. 그래도 돌아가시겠습니까?");
         startActivityForResult(intent, 1);
 
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        if(v==RequestURL)
+        {
+            uploadImage();
+        }
+        else if(v==TimelineButton)
+        {
+            Intent intent = new Intent(this, TimeLine.class);
+            startActivity(intent);
+        }
     }
 
 }
