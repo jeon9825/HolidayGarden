dadf8b2fcb8ef5735e992ccfb16654182993baff
annab96 <lannab082@gmail.com>
2018-07-16 21:21:54 +0900

    test

---
+++ android/app/src/main/java/org/androidtown/holgabun/DbOpenHelper.java
@@ -0,0 +1,88 @@
+package org.androidtown.holgabun;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import org.w3c.dom.Text;
+
+/**
+ * Created by jaery on 2018-05-04.
+ */
+
+public class DbOpenHelper {
+
+    private static final String DATABASE_NAME = "Log.db";
+    private static final int DATABASE_VERSION = 1;
+    public static SQLiteDatabase mDB;
+    private DatabaseHelper mDBHelper;
+    private Context mCtx;
+
+    private class DatabaseHelper extends SQLiteOpenHelper {
+
+        // 생성자
+        public DatabaseHelper(Context context, String name,
+                              SQLiteDatabase.CursorFactory factory, int version) {
+            super(context, name, factory, version);
+        }
+
+        // 최초 DB를 만들때 한번만 호출된다.
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL("CREATE TABLE Log (id Text PRIMARY KEY, password Text,onOff integer);");
+        }
+
+        // 버전이 업데이트 되었을 경우 DB를 다시 만들어 준다.
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS "+DataBase.CreateDB._TABLENAME);
+            onCreate(db);
+        }
+    }
+
+    public DbOpenHelper(Context context){
+        this.mCtx = context;
+    }
+
+    public int returnOnOff()
+    {
+        Cursor cursor= mDB.rawQuery("select onoff from Log",null);
+
+        while(cursor.moveToNext()){
+            return cursor.getInt(0);
+        }
+        return -1;
+    }
+
+    public void automaticLogin()
+    {
+        mDB.execSQL("UPDATE Log set onOff=" + 1 + ";");
+    }
+
+    public void logOut()
+    {
+        mDB.execSQL("UPDATE Log set onOff=" + 0 + ";");
+    }
+
+    public String returnId() {
+        Cursor cursor = mDB.rawQuery("select id from Log", null);
+
+        while (cursor.moveToNext()) {
+            return cursor.getString(0);
+        }
+        return "";
+    }
+
+    public DbOpenHelper open() throws SQLException {
+        mDBHelper = new DatabaseHelper(mCtx, DATABASE_NAME, null, DATABASE_VERSION);
+        mDB = mDBHelper.getWritableDatabase();
+        return this;
+    }
+
+    public void close(){
+        mDB.close();
+    }
+
+}
---
+++ android/app/src/main/java/org/androidtown/holgabun/Search_Password_Popup.java
@@ -0,0 +1,85 @@
+package org.androidtown.holgabun;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.widget.TextView;
+
+
+public class Search_Password_Popup extends Activity {
+    TextView txtText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //타이틀바 없애기
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_search_password_popup);
+
+        //UI 객체생성
+        txtText = (TextView)findViewById(R.id.txtText);
+
+        //데이터 가져오기
+        Intent intent = getIntent();
+        String data = intent.getStringExtra("data");
+        txtText.setText(data);
+    }
+
+    //돌아가기 버튼 클릭
+    public void mOnClose(View v){
+        //데이터 전달하기
+        Intent intent = new Intent();
+        intent.putExtra("result", "Close Popup");
+        setResult(RESULT_OK, intent);
+
+        //액티비티(팝업) 닫기
+        finish();
+    }
+    //건너뛰기 버튼 클릭 ->Main 페이지로 이동
+    public void onClickedToMain(View v){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+   public boolean onTouchEvent(MotionEvent event) {
+        //바깥레이어 클릭시 안닫히게
+        if(event.getAction()==MotionEvent.ACTION_OUTSIDE){
+            return false;
+        }
+        return true;
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        //안드로이드 백버튼 막기
+        return;
+    }
+    public void onClickedTutorial(View v){
+        Intent intent = new Intent(this, TutorialActivity.class);
+        startActivity(intent);
+    }
+
+}
+
+
+/*
+package org.androidtown.holgabun;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PopupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_popup);
+    }
+}
+*/
