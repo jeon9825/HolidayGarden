f27a33e0732c920a038e9ccc25775a146573f888
jeon9825 <jeon9825@naver.com>
2018-07-16 20:12:18 +0900

    0716 [feature/android/0716jiyoon2]

---
+++ android/app/src/main/java/org/androidtown/holgabun/DataBase.java
@@ -0,0 +1,16 @@
+package org.androidtown.holgabun;
+
+        import android.provider.BaseColumns;
+
+public final class DataBase {
+
+    public static final class CreateDB implements BaseColumns{
+        public static final String _TABLENAME = "Log";
+        public static final String _CREATE =
+                "create table "+_TABLENAME+"("
+                        +"id"+" Text primary key, "
+                        +"password"+" Text"
+                        +"onoff"+"integer"
+                        +");";
+    }
+}
